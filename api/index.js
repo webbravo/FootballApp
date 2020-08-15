a45a4492b088d64411f4dbe80de80a4c708c472a
Kenneth Ijama <4natcom@gmail.com>
2020-08-15 06:45:22 +0100

    Edited the connection file for deployment process

--- api/index.js
+++ api/index.js
@@ -1,5 +1,44 @@
-module.exports = (app, checkJWT) => {
-    app.use("/api/users", require("./users/users.routes"));
+module.exports = (app, jwt) => {
+    app.use("/api/authenticate", require("./users/users.controller").authenticate);
+
+    // Set attachUser as Global MiddleWare
+    app.use(attachUser);
+
+
+    // JWT Auth Middleware
+    const checkJWT = jwt({
+        secret: process.env.JWT_SECRET,
+        issuer: "api.orbit",
+        audience: "api.orbit",
+        algorithms: ["HS256"],
+        getToken: (req) => req.cookies.token,
+    });
+
+    app.use("/api/users", require("./users/users.routes")(checkJWT));
+    app.use("/api/rapidapi", require("./rapidAPI/index"));
+
     app.use("/api/live-games", require("./liveGames/games.routes"));
     app.use("/api/prediction", require("./liveGames/games.routes"));
-}
+};
+
+
+const attachUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({
+            message: "Authentication Invalid",
+        });
+    }
+
+
+    const decodedToken = require("jwt-decode")(token);
+
+    if (!decodedToken) {
+        return res.status(401).json({
+            message: "There was a problem authorizing the request!",
+        });
+    } else {
+        req.user = decodedToken;
+        next();
+    }
+};
--- models/connection.js
+++ models/connection.js
@@ -1,37 +1,50 @@
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
 var basename = path.basename(__filename);
 var db = {};
 
+// // Connect to Sqlite DB
+// const sequelize = new Sequelize('db', 'user', 'pass', {
+//     host: "localhost",
+//     dialect: "sqlite",
+//     storage: "db.sqlite",
+//     operatorAliases: false,
+//     define: {
+//         freezeTableName: true,
+//     },
+// });
 
-
-// Connect to Sqlite DB
-const sequelize = new Sequelize('db', 'user', 'pass', {
+// Connect to Mysql
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
     host: "localhost",
-    dialect: "sqlite",
-    storage: "db.sqlite",
-    operatorAliases: false,
+    dialect: process.env.DB_DIALECT,
     define: {
-        freezeTableName: true,
+      freezeTableName: true,
     },
-});
+  }
+);
 
-fs
-    .readdirSync(__dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-        console.log(file);
-        var model = sequelize.import(path.join(__dirname, file));
-        db[model.name] = model;
-    });
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    // console.log(file);
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
-Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
