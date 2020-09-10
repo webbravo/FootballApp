af46f55a0baef504525487365b5ebf98da73e03d
Kenneth Ijama <4natcom@gmail.com>
2020-09-10 15:41:33 +0100

    Update users.routes.js

--- api/users/users.routes.js
+++ api/users/users.routes.js
@@ -1,53 +1,46 @@
-module.exports = () => {
+const express = require('express');
+const controller = require('./users.controller');
+const router = express.Router();
+const validation = require('../../middlewares/validations');
+const {
+    isAuth
+} = require('../../middlewares/isAuth.js');
 
-    const express = require('express');
-    const controller = require('./users.controller');
-    const router = express.Router();
-    const validation = require('../../middlewares/validations');
-    const {
-        isAuth
-    } = require('../../middlewares/isAuth.js');
-
-    // Get all users
-    router.get("/", controller.all);
+// Get all users
+router.get("/", controller.all);
 
 
-    // login a user
-    router.post("/authenticate", validation.loginUser, controller.authenticate);
+// logout a user
+router.post("/logout", controller.logout)
+
+// Display a welcome message
+router.get("/welcome", controller.welcome);
+
+// Protected Route
+router.post("/protected", isAuth, controller.protected);
+
+// Protected Find User Route
+router.post("/protected-user", isAuth, controller.findByIdP);
+
+// Get user by username
+router.get("/:username", controller.findByUsername);
 
 
-    // logout a user
-    router.post("/logout", controller.logout)
+// Update User Record
+router.put("/:id", controller.update)
 
-    // Display a welcome message
-    router.get("/welcome", controller.welcome);
+// Get user by id Route: @Private
+router.get("/id/:id", controller.findById);
 
-    // Protected Route
-    router.post("/protected", isAuth, controller.protected);
+//  Delete user record
+router.delete("/:id", controller.delete);
 
-    // Protected Find User Route
-    router.post("/protected-user", isAuth, controller.findByIdP);
-
-    // Get user by username
-    router.get("/:username", controller.findByUsername);
-
-    // Update User Record
-    router.put("/:id", controller.update)
-
-    // Get user by id Route: @Private
-    router.get("/id/:id", controller.findById);
-
-    //  Delete user record
-    router.delete("/:id", controller.delete);
-
-    // Get Refresh Token
-    router.post('/refresh_token', controller.refreshtoken);
+// Get Refresh Token
+router.post('/refresh_token', controller.refreshtoken);
 
 
-    // Permanently Delete user record
-    // router.delete("/final/:id", controller.delete);
+// Permanently Delete user record
+// router.delete("/final/:id", controller.delete);
 
 
-
-    return router
-}
+module.exports = router;
