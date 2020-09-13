ff70b866a1189c9df5a28bfe6f0a2d7b29b179d4
Kenneth Ijama <4natcom@gmail.com>
2020-09-13 07:10:00 +0100

    Update users.routes.js

--- api/users/users.routes.js
+++ api/users/users.routes.js
@@ -45,37 +45,4 @@ router.post('/refresh_token', controller.refreshtoken);
 module.exports = router;
 
 
-// logout a user
-router.post("/logout", controller.logout)
 
-// Display a welcome message
-router.get("/welcome", controller.welcome);
-
-// Protected Route
-router.post("/protected", isAuth, controller.protected);
-
-// Protected Find User Route
-router.post("/protected-user", isAuth, controller.findByIdP);
-
-// Get user by username
-router.get("/:username", controller.findByUsername);
-
-
-// Update User Record
-router.put("/:id", controller.update)
-
-// Get user by id Route: @Private
-router.get("/id/:id", controller.findById);
-
-//  Delete user record
-router.delete("/:id", controller.delete);
-
-// Get Refresh Token
-router.post('/refresh_token', controller.refreshtoken);
-
-
-// Permanently Delete user record
-// router.delete("/final/:id", controller.delete);
-
-
-module.exports = router;
